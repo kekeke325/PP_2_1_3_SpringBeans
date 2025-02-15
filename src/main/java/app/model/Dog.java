196f9fd36e17d61657388451a38e4f55c0ce62a4
kekeke <muscarin69@gmail.com>
2025-02-15 23:42:33 +0300

    Solution

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -1,11 +1,8 @@
 package app.model;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Component("dog")
-@Primary
+@Component
 public class Dog extends Animal {
     @Override
     public String toString() {
